c23f5580b0bb75ac638c490eee4d3fd27883b8af
Casprine Assempah <casprine.001@gmail.com>
2018-06-23 16:42:47 +0000

    blog-post styling done

--- src/components/Body.vue
+++ src/components/Body.vue
@@ -8,13 +8,25 @@
                 <div class="post-about">Reducing an interface until only the absolutely necessary elements remain is one of the most satisfying tasks in design.</div>
                 <div class="post-social">
                     <div class="tag">#Vue</div>
-                    <div class="twitter">tweet it </div>
+                    <div class="twitter"> tweet it </div>
                 </div>
                 <div class="post-time">
                     Saturday, 23 June 2018
                 </div>
             </div>
-
+            <div class="post">
+                <div class="post-title">
+                    Refactoring Github’s Design
+                </div>
+                <div class="post-about">Reducing an interface until only the absolutely necessary elements remain is one of the most satisfying tasks in design.</div>
+                <div class="post-social">
+                    <div class="tag">#Vue</div>
+                    <div class="twitter"> tweet it </div>
+                </div>
+                <div class="post-time">
+                    Saturday, 23 June 2018
+                </div>
+            </div>
         </div>
     </div>
 </template>
