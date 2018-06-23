bce02b4d1d2383cad07fa34b557b5c4896753c9a
Casprine Assempah <casprine.001@gmail.com>
2018-06-23 03:00:16 +0000

    init commit :wave:

---
+++ src/App.vue
@@ -0,0 +1,8 @@
+<template>
+  <div id="app">
+    <router-view/>
+  </div>
+</template>
+
+<style src="./assets/styles/main.scss" lang="scss">
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="main">
+          <div class="container">
+            <div class="image">
+              <img src="" alt="" srcset="">
+            </div>
+            <div class="name">Griffith</div>
+            <div class="about">Full Stack Javacript devloper</div>
+            <button> Follow </button>
+          </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: "Header"
+};
+</script>
