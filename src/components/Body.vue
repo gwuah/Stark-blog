dd404f36a1ffa39866ccf46ebd6bc23675079d14
Casprine Assempah <casprine.001@gmail.com>
2018-06-23 03:16:28 +0000

    post details done

--- src/components/Body.vue
+++ src/components/Body.vue
@@ -8,8 +8,12 @@
                     <div class="post-title">
                         Refactoring Github’s Design
                     </div>
-                    <div class="post-about">Reducing an interface until only the absolutely 
+                    <div class="post-about">Reducing an interface until only the absolutely
                     necessary elements remain is one of the most satisfying tasks in design.</div>
+                </div>
+                <div class="post-social">
+                    <div class="tag">#Vue</div>
+                    <div class="twitter">tweet it </div>
                 </div>
         </div>
     </div>
